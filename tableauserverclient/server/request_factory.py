7044756176fb8cbbdd37c0d2b865e230324860f4
Jac Fitzgerald <jac.fitzgerald@salesforce.com>
2023-08-01 15:39:53 -0700

    Add site option for attribute-capture
    (Lee's code)

--- tableauserverclient/server/request_factory.py
+++ tableauserverclient/server/request_factory.py
@@ -686,6 +686,8 @@ class SiteRequest(object):
             site_element.attrib["autoSuspendRefreshInactivityWindow"] = str(
                 site_item.auto_suspend_refresh_inactivity_window
             )
+        if site_item.attribute_capture_enabled is not None:
+            site_element.attrib["attributeCaptureEnabled"] = str(site_item.attribute_capture_enabled).lower()
 
         return ET.tostring(xml_request)
 
@@ -790,6 +792,8 @@ class SiteRequest(object):
             site_element.attrib["autoSuspendRefreshInactivityWindow"] = str(
                 site_item.auto_suspend_refresh_inactivity_window
             )
+        if site_item.attribute_capture_enabled is not None:
+            site_element.attrib["attributeCaptureEnabled"] = str(site_item.attribute_capture_enabled).lower()
 
         return ET.tostring(xml_request)
 
